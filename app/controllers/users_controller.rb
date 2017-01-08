5c16403f012cb97d6209295695abfd6a48444297
Ivan Pilot <ivanpilot@gmail.com>
2017-01-08 21:41:32 +0000

    impletemented bootstrap in the application et started a logout function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # raise params.inspect
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "You have successfully signed up"
+      redirect_to user_path(@user)
+    else
+      flas.short[:alert] = "There was an issue during the sign up process"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
