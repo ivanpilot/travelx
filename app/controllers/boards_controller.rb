3356a87143e3f70e26506f268737512640cc89d2
Ivan Pilot <ivanpilot@gmail.com>
2017-01-08 19:08:18 +0000

    added routes to sign in, sign up and log out

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,6 @@
 class BoardsController < ApplicationController
+
+  def index
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  resources :friendships
-  resources :categories
-  # resources :activity_categories
+
+  root to: 'boards#index'
+  get '/sign_up', to: 'users#new'
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :boards
-  # resources :board_activities
   resources :activities
-  # resources :experiences
   resources :users
+
+  
+  # resources :friendships
+  # resources :experiences
+  # resources :board_activities
+  # resources :activity_categories
+  # resources :categories
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
