395c4a5a715f2ae2a201c3b2b71a73d0e4f23908
Ivan Pilot <ivanpilot@gmail.com>
2017-01-09 01:01:25 +0000

    added the login form and action within the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email:params[:user][:email])
+    if user && user.try(:authenticate, params[:user][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are signed in."
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "The combination email/password is not valid."
+      render :new
+    end
   end
 
   def destroy
     session.delete(:user_id)
+    flash[:success] = "You are logged out."
     redirect_to root_path
   end
 
