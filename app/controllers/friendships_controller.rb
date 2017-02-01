cb8ee1bcfbb4871d9521cb950d28d0b90b031354
Ivan Pilot <ivanpilot@gmail.com>
2017-02-01 09:31:31 +0000

    function to add a new friend implemented but need to refactor code

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,24 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user
 
-  
+  def create
+    # raise params.inspect
+    new_friend = User.find_by(username: friendship_params[:friend])
+    if new_friend
+      new_friendship = Friendship.new(user_id: current_user.id, friend_id: new_friend.id)
+      if new_friendship.save
+        flash[:success] = "you added a new friend"
+      else
+        flash[:warning] = "there was an error"
+      end
+    end
+    redirect_to friends_user_path(current_user)
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend)
+  end
 
 end
