fb77b75721cfb647170a44c9acee17573241d7bf
Ivan Pilot <ivanpilot@gmail.com>
2017-02-05 20:24:00 +0000

    refactor index page for boards and implement show page for board including controller action

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,4 +7,18 @@ class BoardsController < ApplicationController
     end
   end
 
+  def show
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      if @user.nil?
+        redirect_to root_path(@user), alert:"Error. User not found."
+      else
+        @board = @user.boards.find_by(id: params[:id])
+        redirect_to user_boards_path(@user), alert:"Board not found." if @board.nil?
+      end
+    else
+      @board = Board.find_by(id:params[:id])
+    end
+  end
+
 end
