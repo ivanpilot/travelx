5287e0a2bfa4be3648119b5f32e715af53c92714
Ivan Pilot <ivanpilot@gmail.com>
2017-02-12 12:33:09 +0000

    refactor edit and update action for activity controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,6 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user
 
   def edit
-    # @activity = Activity.find_by(id: params[:id])
     if params[:user_id]
       @user = User.find_by(id: params[:user_id])
 
@@ -11,7 +10,7 @@ class ActivitiesController < ApplicationController
 
         if @activity.nil?
           flash[:danger] = "Activity not found."
-          redirect_to user_boards_path(@user)
+          redirect_to user_boards_path(current_user)
         end
       else
         redirect_to user_boards_path(current_user)
@@ -27,29 +26,16 @@ class ActivitiesController < ApplicationController
         @activity = @user.activities.find_by(id: params[:id])
 
         if !@activity.nil? && @activity.update(activity_params)
-          # flash[:danger] = "Activity not found."
-          # redirect_to user_boards_path(@user)
-          # if
           flash[:success] = "Activity updated."
-          redirect_to user_boards_path(@user)
-            # redirect_to board_path(@activity.board) #TO BE CHANGEDDDDD to maintain url including user
+          redirect_to user_boards_path(@user)#TO BE CHANGEDDDDD to previous url
         else
           flash[:danger] = "Activity couldn't be found or updated."
-          redirect_to user_boards_path(@user)
+          redirect_to user_boards_path(current_user)
         end
       else
         redirect_to user_boards_path(current_user)
       end
     end
-    # @activity = Activity.find_by(id: params[:id])
-    # if @activity.update(activity_params)
-    #   flash[:success] = "Board updated."
-    #   redirect_back(fallback_location: root_path)
-    #   redirect_to board_path(@activity.board) #TO BE CHANGEDDDDD to maintain url including user
-    # else
-    #   flash.now[:alert] = "Board couldn't be updated."
-    #   render :edit
-    # end
   end
 
   private
