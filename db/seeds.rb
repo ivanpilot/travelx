52a120e1e5e4e78a49625f14a6d1f5181dfaf477
Ivan Pilot <ivanpilot@gmail.com>
2017-02-12 12:55:53 +0000

    created a seed file to run test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Ivan", email: "ivanpilot@gmail.com", password: "password", password_confirmation: "password")
+ivan = User.create(username: "Ivan", email: "ivanpilot@gmail.com", password: "password", password_confirmation: "password")
 User.create(username: "James", email: "james@gmail.com", password: "password", password_confirmation: "password")
 User.create(username: "Pierre", email: "pierre@gmail.com", password: "password", password_confirmation: "password")
 User.create(username: "Jean", email: "jean@gmail.com", password: "password", password_confirmation: "password")
+
+
+ivan.activities.create(description: "Walk in Hyde Park during summer time", rating:"4")
+ivan.activities.create(description: "Winter run for 10km around St Pauls and Trafalgar Square", rating:"5")
+ivan.activities.create(description: "Lunch at the Alain Ducas Dorchester", rating:"5")
+ivan.activities.create(description: "Epiphany cake from Lenotre in Paris", rating:"5")
+
+ivan.boards.create(title: "London Winter 2017")
+ivan.boards.create(title: "Paris Winter 2017")
+
+BoardActivity.create(user_id:"1", activity_id:"1")
+BoardActivity.create(user_id:"1", activity_id:"2")
+BoardActivity.create(user_id:"2", activity_id:"4")
