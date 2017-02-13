884df79633d94b268b0bd4525bb2bac84dc8bbeb
Ivan Pilot <ivanpilot@gmail.com>
2017-02-13 10:46:51 +0000

    refactored activity index view page

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,12 @@
 module ActivitiesHelper
+
+  def boards_related_to(activity, user)
+    if activity.boards.empty?
+      "-"
+    else
+      activity.boards.collect do |board|
+        link_to(board.title, user_board_path(current_user, board))
+      end.join().html_safe
+    end
+  end
 end
