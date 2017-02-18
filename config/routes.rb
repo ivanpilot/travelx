7304390cdc94d2a69746855d09bedd10419f2e91
Ivan Pilot <ivanpilot@gmail.com>
2017-02-18 20:57:59 +0000

    added an admin user section allowing to see and control users

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
 
   scope '/admin', module: 'admin' do
     resources :activities, only: [:index]
+    resources :users, only:[:index]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
