52ae294aebae307baad7800f94531807b5132242
Ivan Pilot <ivanpilot@gmail.com>
2017-03-19 20:50:38 +0000

    Reorganized the view and helpers method so that a conditional view based on policy authorization is decided in the views and not in the helpers methods

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,23 +1,5 @@
 module ActivitiesHelper
 
-  def display_create_activity_form(object,user_visited) ####MUST BE MODIFIED IF ADMIN CREATE
-    if policy(:display).show? #DisplayPolicy.new(pundit_user, object).show?
-      concat content_tag(:h2,"Create a new #{object.wordify.capitalize}")
-      form_for object, :url => url_address_create_activity(user_visited) do |f|
-        concat render :partial => "#{object.wordify}", :locals => {object.wordify.to_sym => f}
-        concat f.submit "Create #{object.wordify.capitalize}", class:"btn btn-primary"
-      end
-    end
-  end
-
-  # def display_edit_form(object, user_visited)
-  #   concat content_tag(:h2,"Update your #{object.wordify.capitalize}")
-  #   form_for object, :url => url_address(object, user_visited) do |f|
-  #     concat render :partial => "#{object.wordify}", :locals => {object.wordify.to_sym => f}
-  #     concat f.submit "Update #{object.wordify.capitalize}", class:"btn btn-primary"
-  #   end
-  # end
-
   def url_address_create_activity(user_visited)
     user_visited.nil? ? activities_path : user_activities_path(user_visited)
   end
@@ -26,24 +8,12 @@ module ActivitiesHelper
     user_visited.nil? ? activity_path(activity) : user_activity_path(user_visited, activity)
   end
 
-  def show_activity_edit_link(activity, user_visited) ###TO BE USED NOT ONLY FOR ACTIVITY
-    link_to("| Edit", edit_activity_link(activity, user_visited)) if policy(activity).edit?
-  end
-
   def edit_activity_link(activity, user_visited) ###TO BE USED NOT ONLY FOR ACTIVITY
     user_visited.nil? ? edit_activity_path(activity) : edit_user_activity_path(user_visited, activity)
   end
 
-  def show_activity_delete_link(activity, user_visited) ###TO BE USED NOT ONLY FOR ACTIVITY
-    link_to("| Delete", delete_activity_link(activity, user_visited),method: :delete, data: {confirm: 'Are you sure?'}) if policy(activity).destroy?
-  end
-
   def delete_activity_link(activity, user_visited) ###TO BE USED NOT ONLY FOR ACTIVITY
     user_visited.nil? ? activity_path(activity) : user_activity_path(user_visited, activity)
-  end
-
-  def show_button_to_import_activity(text, activity, user_visited)
-    button_to(text, import_user_activity_path(user_visited, activity), class:"btn btn-success") if current_friend
   end
 
 end
--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -19,43 +19,8 @@ module BoardsHelper
       "No board"
     else
       activity.boards.collect do |board|
-        show_board_link(board, user_visited)
+        link_to(board.title, board_link(board, user_visited)) if policy(board).show?
       end.join(" | ").html_safe
-    end
-  end
-
-  def display_create_activity_form_from_board(object, user_visited) ####MUST BE MODIFIED IF ADMIN CREATE
-    if policy(:display).show? #DisplayPolicy.new(pundit_user, object).show?
-      concat content_tag(:h3,"Start by creating an #{object.wordify} or ...")
-      form_for object, :url => url_address_create_activity(user_visited) do |f|
-        concat render :partial => "activities/activity_inline", :locals => {activity: f}
-        concat f.submit "Create Activity", class:"btn btn-primary"
-      end
-      #  tag("br")
-      # concat(tag 'br')
-    end
-  end
-
-  def display_create_board_form(object, user_visited)
-    if policy(:display).show? #DisplayPolicy.new(pundit_user, object).show?
-      concat content_tag(:h3,"... Create a #{object.wordify}")
-      concat content_tag(:h5,"Create your #{object.wordify} and add somes activities to it!")
-      form_for object, :url => url_address_create_board(user_visited) do |f|
-        concat render :partial => "board", :locals => {board: f}
-        concat render :partial => "fields_for", :locals => {f: f, wrapper: @board.activities.build(user_id:right_user_id(user_visited))}
-        # fields_for(@activities, object.activities.build(user_id:current_user.id)) do |ff|
-        #   # concat ff.hidden_field :user_id
-        #   render(partial: "activities/activity_inline", :locals => {activity: ff})
-        # end
-        concat f.submit "Create Board", class:"btn btn-primary"
-      end
-    end
-  end
-
-  def display_edit_board(object, user_visited)
-    form_for object, :url => url_address_board(object, user_visited) do |f|
-      concat render :partial => "board", :locals => {board: f}
-      concat f.submit "Update", class: "btn btn-primary"
     end
   end
 
@@ -71,20 +36,8 @@ module BoardsHelper
     user_visited.nil? ? board_path(board) : user_board_path(user_visited, board)
   end
 
-  def show_board_link(board, user_visited)
-    link_to(board.title, board_link(board, user_visited)) if policy(board).show?
-  end
-
   def board_link(board, user_visited)
     user_visited.nil? ? board_path(board) : user_board_path(user_visited, board)
-  end
-
-  def show_board_edit_button(board, user_visited)
-    button_to("Edit title", edit_board_link(board, user_visited), :class => "btn btn-info", method: :get) if policy(board).edit?
-  end
-
-  def show_board_delete_button(board, user_visited)
-    button_to("Delete board", delete_board_link(board, user_visited), :class => "btn btn-danger", method: :delete, data: {confirm: "Are you sure?"}) if policy(board).destroy?
   end
 
   def edit_board_link(board, user_visited) ###TO BE USED NOT ONLY FOR BOARD
