62d820e3c5fb097a3a56bf824012ba282b764e6a
Ivan Pilot <ivanpilot@gmail.com>
2017-06-17 18:42:33 +0100

    modified the board serializer to indicate what authorization type

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,11 +1,19 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes :id, :title, :user_id, :authorized_board?
+  attributes :id, :title, :user_id, :authorized_edit_board?, :authorized_destroy_board?
   # belongs_to :user
   has_many :board_activities
   has_many :activities#, through: :board_activities
 
-  # def test
-  #   "hello"
-  # end
+  def authorized_edit_board?
+    current_user = User.find_by(id: self.scope)
+    user_visited = User.find_by(id: self.object.user_id)
+    BoardPolicy.new(UserContext.new(current_user, user_visited), self.object).edit?
+  end
+
+  def authorized_destroy_board?
+    current_user = User.find_by(id: self.scope)
+    user_visited = User.find_by(id: self.object.user_id)
+    BoardPolicy.new(UserContext.new(current_user, user_visited), self.object).destroy?
+  end
 
 end
