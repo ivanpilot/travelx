21eae1178c1c1c1c2ccff1465cb52b4c657f91a9
Ivan Pilot <ivanpilot@gmail.com>
2017-06-18 11:45:04 +0100

    cleaned board file model

--- app/models/board.rb
+++ app/models/board.rb
@@ -36,14 +36,6 @@ class Board < ApplicationRecord
     self.where('user_id = ? AND lower(title) = lower(?)', user_id, board_title.downcase).first
   end
 
-  # def authorized_board?
-  #   # BoardPolicy::Scope.new(UserContext.new(self.user, nil), Board).resolve
-  #   # binding.pry
-  #   show_me_policy = BoardPolicy.new(UserContext.new(self.user, User.all[2]), self).show?
-  #   # show_me_scope = BoardPolicy::Scope.new(UserContext.new(self.user, User.all[2]), self.class).resolve
-  #
-  # end
-
   # def reject_activities(attributes)
   #   attributes['description'].blank? && attributes['rating'].blank?
   # end
